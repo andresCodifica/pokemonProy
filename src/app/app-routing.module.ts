00ed774e120f26c2b0fa9733f5a82304319137d0
andresCodifica <janska029@gmail.com>
2021-01-21 23:11:05 -0500

    contenedor y form para buscar pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { BuscadorComponent } from './buscador/buscador.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: InicioComponent}
+  {path: '', component: InicioComponent},
+  {path: 'buscar', component: BuscadorComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    BuscadorComponent
   ],
   imports: [
     BrowserModule,
