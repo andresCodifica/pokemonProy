476164307864f624c0f1e7d8263a17ebe84dceff
andresCodifica <janska029@gmail.com>
2021-02-08 21:14:35 -0500

    ajustes detalle pokemon

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Observable } from 'rxjs';
 import { Pokemon } from '../models/Pokemon';
 import { PokemonDataService } from '../services/pokemon-data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
@@ -14,16 +14,42 @@ export class DetalleComponent implements OnInit {
   currentPokemon : Pokemon;
 
   constructor(
-    pokemonDataService: PokemonDataService,
+    private pokemonDataService: PokemonDataService,
+    private router: Router,
     rutaActiva: ActivatedRoute) {
     pokemonDataService.getPokemonData(rutaActiva.snapshot.params.item).subscribe(
       (pokemon) => {
+        console.log(pokemon)
         this.currentPokemon = pokemon;
-        console.log(pokemon.abilities)
+     },
+     (error) => {      
+      this.router.navigate(['/buscar/'])
+       alert("No se encontro pokemon.")  
+              
+     })
+  }
+
+  nextPokemon(num: any){
+    let pokemonNumber = parseInt(num)
+    if(num!=200){pokemonNumber++}
+    this.pokemonDataService.getPokemonData(pokemonNumber.toString()).subscribe(
+      (pokemon) => {
+        this.currentPokemon = pokemon;
+        console.log(pokemon)
      }
     )
   }
+  previousPokemon(num: any){
+    let pokemonNumber = parseInt(num)
+    if(num!=0){pokemonNumber--}
+    this.pokemonDataService.getPokemonData(pokemonNumber.toString()).subscribe(
+      (pokemon) => {
+        this.currentPokemon = pokemon;
+        console.log(pokemon)
+     }
+    )
 
+  }
   ngOnInit(): void {
   }
 
